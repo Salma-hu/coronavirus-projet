e5dce60e951a7da5bacb785aa60327c2a52a3369
Salma Belassiri <salmabelassiri@gmail.com>
2020-05-09 16:42:49 +0000

    stepper and progress bar

--- Starter-file/app/js/test.js
+++ Starter-file/app/js/test.js
@@ -3,20 +3,20 @@ var currentTab = 0;
 showTab(currentTab);
 
 function showTab(n) {
-	
-	
+    
+    
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
   
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
   } else {
-    document.getElementById("prevBtn").style.display = "inline";
+    document.getElementById("prevBtn").style.display = "none";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("nextBtn").style.display = "none";
   } 
-	else {
+    else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
   fixStepIndicator(n)
